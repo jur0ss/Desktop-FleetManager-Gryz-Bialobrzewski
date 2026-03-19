47c3092e576a9f9de86125f774df4b48faee0c03
jurekg <jurekgz27@gmail.com>
2026-03-19 10:14:18 +0100

    Stworzono klasę Vehicle z Poziomem Paliwa i Statusem

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -1,6 +1,28 @@
+using System;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+
 namespace FleetManager.Models;
 
-public class Vehicle
+public class Vehicle : ReactiveObject
 {
     
+    [Reactive] public string Name { get; set; } = string.Empty;
+    
+    [Reactive] public string RegNumber { get; set; } = string.Empty;
+    
+    private int _fuelLevel;
+    public int FuelLevel
+    {
+        get => _fuelLevel;
+        set
+        {
+            var clamped = Math.Clamp(value, 0, 100);
+            this.RaiseAndSetIfChanged(ref _fuelLevel, clamped);
+        }
+    }
+    
+    [Reactive] public VehicleStatus Status { get; set; } = VehicleStatus.Available;
+
 }
