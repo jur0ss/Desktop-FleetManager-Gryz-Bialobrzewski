68bce027b9b656f9b3198a8dd848f707e1aba7d9
jurekg <jurek.gryz2007@gmail.com>
2026-04-30 00:20:12 +0200

    Stworzenie modelu czytelnej karty na pojazdy

--- FleetManager/ViewModels/VehicleItemViewModel.cs
+++ FleetManager/ViewModels/VehicleItemViewModel.cs
@@ -1,32 +1,54 @@
-// (ViewModel dla UserControl)
-
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Text.Json;
+using System;
+using System.Reactive;
 using ReactiveUI;
 using FleetManager.Models;
 
 namespace FleetManager.ViewModels;
 
-public class VehiclesViewModel : ReactiveObject
+public class VehicleItemViewModel : ReactiveObject
 {
-    public ObservableCollection<Vehicle> Vehicles { get; } = new();
+    public Vehicle Vehicle { get; }
 
-    public VehiclesViewModel()
+    public ReactiveCommand<Unit, Unit> RefuelCommand { get; }
+    public ReactiveCommand<Unit, Unit> SendInRouteCommand { get; }
+    public ReactiveCommand<Unit, Unit> SetAvailableCommand { get; }
+    public ReactiveCommand<Unit, Unit> SetServiceCommand { get; }
+
+    public VehicleItemViewModel(Vehicle vehicle)
     {
-        LoadVehicles();
+        Vehicle = vehicle;
+
+        RefuelCommand = ReactiveCommand.Create(Refuel);
+        SendInRouteCommand = ReactiveCommand.Create(SendInRoute);
+        SetAvailableCommand = ReactiveCommand.Create(() => Vehicle.Status = VehicleStatus.Available);
+        SetServiceCommand = ReactiveCommand.Create(() => Vehicle.Status = VehicleStatus.Service);
     }
 
-    private void LoadVehicles()
+    private void Refuel()
     {
-        var json = File.ReadAllText("vehicles.json");
-        var vehicles = JsonSerializer.Deserialize<List<Vehicle>>(json);
-
-        if (vehicles is not null)
+        if (Vehicle.Status == VehicleStatus.InRoute)
         {
-            foreach (var v in vehicles)
-                Vehicles.Add(v);
+            Console.WriteLine("Pojazd jest niedostępny.");
+            return;
         }
+
+        Vehicle.FuelLevel = 100;
+    }
+
+    private void SendInRoute()
+    {
+        if (Vehicle.Status == VehicleStatus.Service)
+        {
+            Console.WriteLine("Pojazd jest w serwisie.");
+            return;
+        }
+
+        if (Vehicle.FuelLevel < 15)
+        {
+            Console.WriteLine("Pojazd ma za mało paliwa.");
+            return;
+        }
+
+        Vehicle.Status = VehicleStatus.InRoute;
     }
 }
